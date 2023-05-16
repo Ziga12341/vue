a2a7da4706d156c955580d68a2e46233a24f807a
Ziga Pregelj - Asus <ziga.pregelj@razum.si>
2023-05-16 15:45:05 +0200

    scss and hover done

---
+++ child-components/components/Greeting.vue
@@ -0,0 +1,20 @@
+<template>
+  <p> {{msg}} </p>
+</template>
+
+<script>
+export default {
+  name: "Greetings",
+  data() {
+    return {
+        msg: "Hello World!!!!"
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss">
+p:hover {
+  color: darken(#cc4444, 15%);
+}
+</style>
---
+++ child-components/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>Hey!</p>
+  <greeting></greeting>
+</template>
+
+<script>
+import Greeting from '../components/Greeting.vue'
+export default {
+  name: 'App',
+  components: {
+      Greeting
+  }
+}
+</script>
