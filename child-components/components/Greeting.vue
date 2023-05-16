e8426809df110fed6b99f8b649e89b3664fddd9a
Ziga Pregelj - Asus <ziga.pregelj@razum.si>
2023-05-16 16:25:39 +0200

    update age button works

--- child-components/components/Greeting.vue
+++ child-components/components/Greeting.vue
@@ -1,10 +1,12 @@
 <template>
-  <p> {{msg}} </p>
+  <p v-if="age > 25"> {{msg}} </p>
+  <p v-else>You must be 25 years old or older to view this message</p>
 </template>
 
 <script>
 export default {
   name: "Greetings",
+  props: ["age"],
   data() {
     return {
         msg: "Hello World!!!!"
