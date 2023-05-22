b60c7ac61321b0da331562c3a158f48143f4e260
Ziga Pregelj - Asus <ziga.pregelj@razum.si>
2023-05-22 21:37:30 +0200

    reset button works

--- child-components/components/User.vue
+++ child-components/components/User.vue
@@ -1,12 +1,21 @@
 <template>
   <button type="button" @click="onClickAge">Update Age</button>
-  <p>The user is {{ age }}years old</p>
-  <p>The double user age is {{ ageDoubled }}years old</p>
+  <p>The user is {{ age }} years old</p>
+  <p>The double user age is {{ ageDoubled }} years old</p>
 </template>
 <script>
 export default {
   name: "User",
-  props: ["age"],
+  props: {
+      age: {
+        type: Number,
+        // required: true,
+        default: 20,
+        validator(value) {
+            return value >= 130
+        }
+    }
+  },
   emits: ['age-change'],
   computed: {
     ageDoubled() {
